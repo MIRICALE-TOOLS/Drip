eda436993533642005f26c330180d0b2acfda4c2
xshonda <qdude035@gmail.com>
2021-11-26 16:40:28 -0600

    better url shortner + discord id lookup
    Improved URL Shortner
    Added Discord ID Lookup
    Added MOTDs
    Organized Code

--- main.py
+++ main.py
@@ -2,18 +2,20 @@
 import requests, os, ctypes, json, pyperclip, time
 from colorama import Fore, init
 
+
 ##Global Variables
-cmd = os.system
 color = Fore
-p = print
+
+
 ##Convert Colorama for Windows Command Processors
 init(convert=True)
 
+
 ##CLI Logo
 def logo():
-    cmd('cls')
-    ctypes.windll.kernel32.SetConsoleTitleW("API Dawg V0.5")
-    p(color.YELLOW +"""
+    os.system('cls')
+    ctypes.windll.kernel32.SetConsoleTitleW("API Dawg V0.6")
+    print(color.YELLOW +"""
       ___  ______ _____  ______  ___  _    _ _____ 
      / _ \ | ___ \_   _| |  _  \/ _ \| |  | |  __ \\
     / /_\ \| |_/ / | |   | | | / /_\ \ |  | | |  \/
@@ -24,56 +26,183 @@ def logo():
                                                
     """ + color.LIGHTYELLOW_EX)
 
-##CLI Menus
+
+##Main Menu
 def menu():
     logo()
-    p("""
+    motd()
+    print("""
     [1] Module List
     [2] About
     [X] Exit
     """)
+    
     choice = input("")
+
+
     if choice == '1':
         modules()
+
+
     elif choice == '2':
-        p("coming soon")
+        logo()
+        print("""
+        I'm making this tool to expand my knowledge on the following topics: Web APIs, JSON & Web Requests
+
+
+                        this tool was developed by: xshonda#9999 | github.com/xshonda
+        """)
+        time.sleep(10)
         menu()
+
+
     elif choice == 'x':
         exit
+
+
     else:
         menu()
 
+
+##Module List
 def modules():
     logo()
-    p("""
+    print("""
     [1] URL Shortner
-    [2] Password Generator
-    [3] Discord ID Lookup
+    [2] Discord ID Lookup
     [<] Go Back
     """)
+
     choice = input("")
+
+
     if choice == '1':
         urlshort()
+
+
     elif choice == '2':
-        modules()
-    elif choice == '3':
-        modules()
+        discordlookup()
+
+
     elif choice == '<':
         menu()
 
+
+    else: 
+        modules()
+
+
 ##URL Shortner
 def urlshort():
-    link = input("URL: ")
-    url = "https://owo.vc/generate"
-    data = {"link": f"{link}",
-    "generator": "owo",
-    "preventScrape": "true",
-    "owoify": "true"}
-    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    r = requests.post(url, data=json.dumps(data), headers=headers)
+    logo()
+    print("""
+    [1] owo.vc
+    [2] 1pt.co
+    [3] cleanuri.com
+    [<] Go Back
+    """)
+
+    choice = input("Service: ")
+
+
+    if choice == '1':
+        logo()
+
+        link = input("URL: ")
+
+        url = "https://owo.vc/generate"
+        data = {"link": f"{link}",
+        "generator": "owo",
+        "preventScrape": "true",
+        "owoify": "true"}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        response = r.json()
+        
+        print("Shortened URL (Copied to Clipboard): " + response['result'])
+
+        pyperclip.copy(response['result'])
+        time.sleep(3)
+        modules()
+
+
+    elif choice == '2':
+        logo()
+
+        link = input("URL: ")
+        short = input("Custom URL: ")
+
+        url = f"https://api.1pt.co/addURL?long={link}&short={short}"
+        payload={}
+        headers = {}
+
+        r = requests.request("GET", url, headers=headers, data=payload)
+        response = r.json()
+        
+        print("Shortened URL (Copied to Clipboard): 1pt.co/" + response['short'])
+
+        pyperclip.copy("1pt.co/" + response['short'])
+        time.sleep(3)
+        modules()
+
+
+    elif choice == '3':
+        logo()
+
+        link = input("URL: ")
+
+        url = "https://cleanuri.com/api/v1/shorten"
+        data = {"url": f"{link}"}
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+
+        r = requests.post(url, data=json.dumps(data), headers=headers)
+        response = r.json()
+        
+        print("Shortened URL (Copied to Clipboard): " + response['result_url'])
+
+        pyperclip.copy(response['result_url'])
+        time.sleep(3)
+        modules()
+
+
+    elif choice == '<':
+        modules()
+
+
+    else:
+        urlshort()
+
+
+def discordlookup():
+    logo()
+
+    id = input('Discord ID: ')
+
+    url = f"https://api.leaked.wiki/discorduser?json=yes&id={id}"
+    payload = {}
+    headers = {'Content-Type': 'text/plain'}
+
+    r = requests.request("GET", url, headers=headers, data=payload)
     response = r.json()
-    print("Shortened URL (Copied to Clipboard): " + response['result'])
-    pyperclip.copy(response['result'])
-    time.sleep(3)
+
+    print('Username: ' + response['username'] + '#' + response['discriminator'])
+    print('Avatar URL: ' + response['avatar'])
+
+    time.sleep(5)
     modules()
+
+
+##Message of the Day (Random Anime Quotes)
+def motd():
+    url = "https://animechan.vercel.app/api/random"
+    payload={}
+    headers = {}
+
+    r = requests.request("GET", url, headers=headers, data=payload)
+    response = r.json()
+
+    print(response['quote']+" - "+response['character'])
+
+
 menu()
