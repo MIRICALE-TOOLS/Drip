c1d9af252d0eb754b9abbdb355b73ce0878487f8
xshonda <93495400+xshonda@users.noreply.github.com>
2021-11-25 16:02:03 -0800

    initial upload (v0.5)

---
+++ main.py
@@ -0,0 +1,76 @@
+##Imports
+import requests, os, ctypes, json, pyperclip, time
+from colorama import Fore, init
+
+##Global Variables
+cmd = os.system
+color = Fore
+p = print
+##Convert Colorama for Windows Command Processors
+init(convert=True)
+
+##CLI Logo
+def logo():
+    cmd('cls')
+    ctypes.windll.kernel32.SetConsoleTitleW("API Dawg V0.1")
+    p(color.YELLOW +"""
+    ______   __    __     ______  
+   /\  == \ /\ "-./  \   /\__  _\ 
+   \ \  _-/ \ \ \-./\ \  \/_/\ \/ 
+    \ \_\    \ \_\ \ \_\    \ \_\ 
+     \/_/     \/_/  \/_/     \/_/ 
+    """ + color.LIGHTYELLOW_EX)
+
+##CLI Menus
+def menu():
+    logo()
+    p("""
+    [1] Module List
+    [2] About
+    [X] Exit
+    """)
+    choice = input("")
+    if choice == '1':
+        modules()
+    elif choice == '2':
+        p("coming soon")
+        menu()
+    elif choice == '3':
+        exit
+    else:
+        menu()
+
+def modules():
+    logo()
+    p("""
+    [1] URL Shortner
+    [2] Password Generator
+    [3] Discord ID Lookup
+    [<] Go Back
+    """)
+    choice = input("")
+    if choice == '1':
+        urlshort()
+    elif choice == '2':
+        modules()
+    elif choice == '3':
+        modules()
+    elif choice == '<':
+        menu()
+
+##URL Shortner
+def urlshort():
+    link = input("URL: ")
+    url = "https://owo.vc/generate"
+    data = {"link": f"{link}",
+    "generator": "owo",
+    "preventScrape": "true",d
+    "owoify": "true"}
+    headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
+    response = r.json()
+    print("Shortened URL (Copied to Clipboard): " + response['result'])
+    pyperclip.copy(response['result'])
+    time.sleep(3)
+    modules()
+menu()
